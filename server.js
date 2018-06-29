0a536a6db0e6653f5f424dbda36efb91a968e6ee
Jake Kopulsky <jakekopulsky@gmail.com>
2018-06-29 12:59:27 -0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path  = require('path');
 const app = express();
 
-// app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/', express.static(path.join(__dirname, './dist')));
 
-app.listen(3000, () => console.log('Demo listening on port 3000!'));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
